884fa0dc6ad343a09f638aa60eaf49bb549637b9
Seonoh Eam <eso0609@naver.com>
2021-02-05 15:44:05 +0900

    add Dao, Repository, ViewModel

--- app/src/main/java/com/example/seonoh/roomwordsample/data/Word.kt
+++ app/src/main/java/com/example/seonoh/roomwordsample/data/Word.kt
@@ -1,4 +1,8 @@
 package com.example.seonoh.roomwordsample.data
 
-class Word {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word : String)
