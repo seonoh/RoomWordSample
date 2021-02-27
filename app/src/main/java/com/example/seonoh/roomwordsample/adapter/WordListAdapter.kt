8474d02f920cb43788e82fc633209b982f3dd5db
Seonoh Eam <eso0609@naver.com>
2021-02-27 18:28:47 +0900

    code review

--- app/src/main/java/com/example/seonoh/roomwordsample/adapter/WordListAdapter.kt
+++ app/src/main/java/com/example/seonoh/roomwordsample/adapter/WordListAdapter.kt
@@ -12,38 +12,5 @@ import com.example.seonoh.roomwordsample.data.Word
 
 class WordListAdapter : ListAdapter<Word, WordListAdapter.WordViewHolder>(WordsComparator()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        return WordViewHolder.create(parent)
-    }
 
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-        val current = getItem(position)
-        holder.bind(current.word)
-    }
-
-    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val wordItemView: TextView = itemView.findViewById(R.id.textView)
-
-        fun bind(text: String?) {
-            wordItemView.text = text
-        }
-
-        companion object {
-            fun create(parent: ViewGroup): WordViewHolder {
-                val view: View = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.recyclerview_item, parent, false)
-                return WordViewHolder(view)
-            }
-        }
-    }
-
-    class WordsComparator : DiffUtil.ItemCallback<Word>() {
-        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
-            return oldItem === newItem
-        }
-
-        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
-            return oldItem.word == newItem.word
-        }
-    }
 }
